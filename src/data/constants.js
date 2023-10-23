01bcac626d3c9f6c8bbb34205428b8709086805b
davidgrimesb <148578135+davidgrimesb@users.noreply.github.com>
2023-10-23 12:27:58 -0400

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,199 @@
+export const Bio = {
+  name: "David Grimes",
+  roles: [
+    "Full Stack Developer",
+    "IOS Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/davidgrimesb",
+  resume:
+    "https://drive.google.com/file/d/1hfFQmQiPD5NE9vtF779Q2ZgmJMGoTbLS/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/david-grimes-a200401b7/",
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+     
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+     
+      
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "SQL Developer",
+        image:
+          "https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Oracle_SQL_Developer_logo.svg/1200px-Oracle_SQL_Developer_logo.svg.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://ih1.redbubble.net/image.416406974.0438/st,small,507x507-pad,600x600,f8f8f8.u4.jpg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+     
+    ],
+  },
+  {
+    title: "Mobile Development",
+    skills: [
+      {
+        name: "Swift",
+        image:
+          "https://developer.apple.com/swift/images/swift-og.png",
+      },
+      
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "Adobe Creative Suite",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Adobe_Creative_Cloud_rainbow_icon.svg/2101px-Adobe_Creative_Cloud_rainbow_icon.svg.png",
+      },
+      {
+        name: "Microsoft Office",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Database Intern",
+    company: "Fulton County",
+    date: "Jan 2023 - May 2023",
+    desc: "Interned at the Records Center in Fulton County, and worked on a project using microsoft access to create a database for government records that could be located with barcode integration.  ",
+    skills: [
+      "Microsoft Access",
+      "SQL",
+      "UI Design",
+      "Barcode Integration",
+    ],
+    
+  },
+  {
+    id: 0,
+    img: "https://rm-core-production.s3.amazonaws.com/logos/3276/large_kroger.jpg",
+    role: "Customer Service",
+    company: "Kroger",
+    date: "November 2018 - Present",
+    desc: "Assisted customers in a retail setting, and provide friendly and knowledgeable customer service to ensure a positive shopping experience.",
+    skills: [
+      "Teamwork",
+      "Problem Solving",
+      "Organization",
+      "Sales",
+    ],
+    
+  },
+  
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://commkit.gsu.edu/files/2019/07/logoLine3-RGB-e1563220704572.jpg",
+    school: "Georgia State Uninversity",
+    date: "Jun 2019 - May 2023",
+    grade: "3.2 GPA",
+    desc: "I graduated with a Bachelor's degree in Computer Information Systems at Georgia State University. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Business - Computer Information Systems",
+  },
+  {
+    id: 1,
+    img: "https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/gwinnett_technical_c/standard.png?1576631034",
+    school: "Gwinnett Technical College",
+    date: "Aug 2017 - May 2019",
+    grade: "3.4 GPA",
+    desc: "I completed my associates degree at Gwinnett Tech where I studied Computer Science and learned skills C++, Swift, and HTML.",
+    degree: "Associate of Science - Computer Science",
+  },
+  
+];
+
+export const projects = [
+
+ 
+  {
+    id: 7,
+    title: "PicPlace App",
+    date: "Jun 2022 - July 2022",
+    description:
+      "A swift app made with Xcode that implements mapkit. It has a collection of hidden photo locations located throughout Atlanta. It also has a learn more section which includes details and information about the selected location.",
+    image:
+      "https://raw.githubusercontent.com/bulaydavid/resumeimages/b46f067be656f1a03ce44ff0b24eb077061c853b/PP.gif",
+    tags: ["Swift", "Xcode", "MapKit"],
+    category: "mobile app",
+    github: "https://github.com/davidgrimesb/PicPlace",
+    webapp: "https://github.com/davidgrimesb/PicPlace",
+  },
+  
+];
+
+
